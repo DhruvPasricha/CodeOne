d8eea8c16d758c7c371782cdff2be4f568b56b56
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-11-08 17:56:08 +0530

    add languages

---
+++ src/components/Languages.jsx
@@ -0,0 +1,1 @@
+export const languages = ["C", "Java", "Python", "Ruby", "Kotlin", "Swift"];
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, NavDropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faCode } from "@fortawesome/free-solid-svg-icons";
+import { languages } from "./Languages";
 
 export default function NavbarComp() {
+  const [currentLang, setCurrentLang] = useState("C++ ");
+  const arr = [];
+
+  function handleClick(newLang, index) {
+    let temp = currentLang;
+    setCurrentLang(newLang);
+    languages[index] = temp;
+    languages.sort();
+  }
+
+  languages.sort();
+  for (let i = 0; i < languages.length; i++) {
+    languages[i] += " ";
+    arr.push(
+      <NavDropdown.Item onClick={() => handleClick(languages[i], i)}>
+        {languages[i]}
+      </NavDropdown.Item>
+    );
+  }
+
+  const logo = <FontAwesomeIcon icon={faCode} size="3x" color="white" />;
+
   return (
     <div>
       <div className="navbar p-3">
-        <Col className="col-10">
+        <div>{logo}</div>
+        <Col className="col-9">
           <span id="name">DR IDE</span>
         </Col>
         <Col className="col-1">
           <div>
-            <NavDropdown title="C++" menuVariant="dark" id="language">
-              <NavDropdown.Item>Python</NavDropdown.Item>
-              <NavDropdown.Item>JS</NavDropdown.Item>
-              <NavDropdown.Item>HTML</NavDropdown.Item>
+            <NavDropdown title={currentLang} menuVariant="dark" id="language">
+              {arr}
             </NavDropdown>
           </div>
         </Col>
