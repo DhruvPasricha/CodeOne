8bd8e59a9dcbde3a8e06d1893ac958b1206de9ca
DhruvPasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-11-08 16:49:51 +0530

    add run icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,28 @@
-import Button from '@restart/ui/esm/Button'
-import React from 'react'
-import { Col, NavDropdown } from 'react-bootstrap'
+import React from "react";
+import { Col, NavDropdown } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavbarComp() {
   return (
     <div>
-      <div className='navbar p-3'>
-        <Col className='col-1'>
-          <span id='name'>DR IDE</span>
+      <div className="navbar p-3">
+        <Col className="col-10">
+          <span id="name">DR IDE</span>
         </Col>
-        <Col>
-          <NavDropdown title='C++' menuVariant='dark' id='language'>
-            <NavDropdown.Item>Python</NavDropdown.Item>
-            <NavDropdown.Item>JS</NavDropdown.Item>
-            <NavDropdown.Item>HTML</NavDropdown.Item>
-          </NavDropdown>
+        <Col className="col-1">
+          <div>
+            <NavDropdown title="C++" menuVariant="dark" id="language">
+              <NavDropdown.Item>Python</NavDropdown.Item>
+              <NavDropdown.Item>JS</NavDropdown.Item>
+              <NavDropdown.Item>HTML</NavDropdown.Item>
+            </NavDropdown>
+          </div>
         </Col>
-        <span id='runButton' className='justify-content-end'>
-          Run
-        </span>
+        <div id="runButton" className="justify-content-end">
+          <FontAwesomeIcon icon={faPlay} size="sm" /> Run
+        </div>
       </div>
     </div>
-  )
+  );
 }
