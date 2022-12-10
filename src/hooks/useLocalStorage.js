fb1572d2ff7d4ce04f4049946b811e2958de672b
DhruvPasricha <dhruvpasricha0002@gmail.com>
2022-12-10 11:54:51 +0530

    update local-storage `PREFIX` constant

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-const PREFIX = 'codeone'
+const PREFIX = 'CODEONE-'
 
 export default function useLocalStorage(key, initialValue) {
   const prefixedKey = PREFIX + key
